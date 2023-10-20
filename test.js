cafa7f7016a30bf250661ede695ef42260896391
Kai Schallenberg <hello@kaischallenberg.de>
2023-10-20 12:31:07 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,12 @@ SLIVE.on("ready", (data) => {
     console.log("\nready")
     console.log(data)
 
+    // SLIVE.config.set({
+    //     id: "t_rtl_logo",
+    //     width: 1920,
+    //     height: 1080,
+    // })
+
     SLIVE.db.set("test", "sdfsdf")
     
     SLIVE.db.get("test", (data) => {
