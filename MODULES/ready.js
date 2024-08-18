4e8468b7affd707ac564288036f1c3b5ec804fa7
Natro-Kai <hello@kaischallenberg.de>
2024-08-18 22:04:58 +0200

    chore: Add websocket controller to ready module

--- MODULES/ready.js
+++ MODULES/ready.js
@@ -1,5 +1,11 @@
 const bodyInjector = require("./bodyInjector")
+const cache = require("./cache")
+const BACKEND_DASHBOARD = require("./CONNECTOR/dashboard")
 
 module.exports = () => {
     bodyInjector.show()
+
+    if(cache.controller) {
+        BACKEND_DASHBOARD.start()
+    }
 }
